6670a08bf5d886a467a56af94c680b4f25a9eb6d
n0s028a <namesh.singh@walmart.com>
2024-11-16 12:01:02 +0530

    app changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!!');
+	res.send('Hello Namesh!!');
 })
 
 app.listen(port, () => console.log(`App listening no port ${port}`));
