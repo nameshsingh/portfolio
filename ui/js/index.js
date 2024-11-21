c70b4694150938f1e6e17f03b34ecd495edb5a0a
n0s028a <namesh.singh@walmart.com>
2024-11-21 13:44:44 +0530

    wss url change

--- ui/js/index.js
+++ ui/js/index.js
@@ -15,7 +15,7 @@ try {
   if (['localhost', '127.0.0.1', ''].includes(location.hostname)) {
     ws = new WebSocket(`ws://localhost:3000`);
   } else {
-    ws = new WebSocket(`wss://jemisthe.best`);
+    ws = new WebSocket(`wss://nameshsingh.com`);
   }
 } catch(e) {
   console.log('Web socket init error', e);
