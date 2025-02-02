1ce88fbff41f98a7611268b31ce958417bd87ea5
n0s028a <namesh.singh@walmart.com>
2025-02-02 15:14:40 +0530

    update nextconfign

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+  output: "standalone", // This enables the standalone output
 };
 
 export default withContentlayer(nextConfig);
